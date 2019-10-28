4ef3be36a531d3d1c8cb11b29684b070d14bb73f
Ra0l <45242019+Ra0l@users.noreply.github.com>
2019-10-28 15:43:54 -0500

    Primer commit

---
+++ Calculator/Models/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    class Calculadora
+    {
+        public int currentState { get; set; }
+        public int mathOperator { get; set; }
+        public int firstNumber { get; set; }
+        public int secondnumber { get; set; }
+    }
+}
